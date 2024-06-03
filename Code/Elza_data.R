beb68ddd14bc3ba34fd51fbbe4e6cee268ada0d5
Ismael Soto <isma-sa@hotmail.com>
2024-06-03 21:06:46 +0200

    Update Elza_data.R

--- Code/Elza_data.R
+++ Code/Elza_data.R
@@ -3,7 +3,7 @@
 ################################################################
 
 
-setwd("C:/Users/isma-/OneDrive/Escritorio/Elza_data")
+setwd("C:/Users/Propietario/Desktop/Escritorio/Elza_data")
 df<- read_excel("DATA FOR ISMAEL.xlsx")
 df1<- read_excel("DATA FOR ISMAEL.xlsx", sheet = "Sampling Geomar 2022")
 df2<- read_excel("DATA FOR ISMAEL.xlsx", sheet = "Sampling Trave 2022")
@@ -290,7 +290,7 @@ data1<- data %>% group_by(`Sampling month`, origin, location) %>%
 data1
 
 month_order <- c("April", "May", "June", "July", "August", "September", 
- "October", "November", "December", "January", "February", "March")
+                 "October", "November", "December", "January", "February", "March")
 
 data1$`Sampling month` <- factor(data1$`Sampling month`, levels = month_order)
 
@@ -302,13 +302,29 @@ data1$location[data1$location=="Falkestein"] <- "Falckenstein beach"
 data1$location[data1$location=="Geomar"] <- "Downtown Kiel"
 data1$location[data1$location=="Trave"] <- "Dassower See"
 
+
+data$origin[data$origin=="alien"] <- "non-native"
+data$origin[data$origin=="non-native"] <- "Non-native amphipods"
+data$origin[data$origin=="native"] <- "Native amphipods"
+
+data$location[data$location=="Falkestein"] <- "Falckenstein beach"
+data$location[data$location=="Geomar"] <- "Downtown Kiel"
+data$location[data$location=="Trave"] <- "Dassower See"
+
+
 location <- c("Falckenstein beach", "Downtown Kiel", "Dassower See")
 data1$location <- factor(data1$location, levels = location)
 
 
+data1$location <- factor(data1$location)
+data$location <- factor(data$location, levels = levels(data1$location))
 
+color_mapping <- c("Native amphipods" = "blue", "Non-native amphipods" = "red")
+data <- data %>% filter(Abundance> 0)
 p2<- ggplot(data1, aes(`Sampling month`, Total_abundance, group=as.factor(origin)))  +
-  geom_line(aes(colour=origin), size=1) + theme_cleveland()+ theme_classic2()+
+  geom_line(aes(colour=origin), size=1) + 
+  geom_point(data = data, aes(x = `Sampling month`, y = Abundance, colour = origin)) + 
+   theme_cleveland()+ theme_classic2()+   scale_color_manual(values = color_mapping) + 
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1,color="black"))+
   theme(axis.text.y = element_text(color="black"))+ 
   scale_y_continuous(breaks = seq(0, max(data1$Total_abundance) + 50, by = 50))+
@@ -363,7 +379,7 @@ data2<- data1 %>% group_by(Months, location) %>%
 
 data$`Sampling Date` <- as.Date(data$`Sampling Date`, format = "%d/%m/%Y")
 data$`Sampling month` <- factor(data$`Sampling month`, levels = c("April", "May", "June", "July", "August", "September",
-                                                                "October", "November", "December", "January", "February", "March"))
+                                                                  "October", "November", "December", "January", "February", "March"))
 
 data$Year <- format(data$`Sampling Date`, "%Y")
 
@@ -373,8 +389,15 @@ abundance_data <- data %>%
                 location = location, Abundance = Abundance) %>%
   arrange(`Sampling Date`)
 
+abundance_data1 <- abundance_data %>% group_by(`Sampling Date`, Year,location) %>%
+  summarise(Abundance =sum(Abundance))
+colnames(abundance_data1)[1] <- "Date"
+abundance_data1$Date <- as.factor(abundance_data1$Date)
+abundance_data1$Date <- as.Date(as.character(abundance_data1$Date))
+abundance_data1$Date_num <- as.numeric(difftime(abundance_data1$Date, min(abundance_data1$Date), units = "days"))
 
 
+abundance_data$location <- as.character(abundance_data$location)
 
 model_gammarus <- list()
 for (loc in unique(abundance_data$location)) {
@@ -384,34 +407,36 @@ for (loc in unique(abundance_data$location)) {
   local_data1$Year <- as.factor(local_data1$Year)
   colnames(local_data1)[1] <- "Date"
   local_data1 <- dplyr::ungroup(local_data1)
-local_data1$Date <- as.factor(local_data1$Date)
-local_data1$Date <- as.Date(as.character(local_data1$Date))
-local_data1$Date_num <- as.numeric(difftime(local_data1$Date, min(local_data1$Date), units = "days"))
-
-model_gammarus[[loc]] <- gam(Abundance ~ s(Date_num, bs = "cs") + Year, 
-                             data = local_data1, 
-                             family = nb())
+  local_data1$Date <- as.factor(local_data1$Date)
+  local_data1$Date <- as.Date(as.character(local_data1$Date))
+  local_data1$Date_num <- as.numeric(difftime(local_data1$Date, min(local_data1$Date), units = "days"))
+  
+  model_gammarus[[loc]] <- gam(Abundance ~ s(Date_num, bs = "cs") + Year, 
+                               data = local_data1, 
+                               family = nb())
 }
-summary(model_gammarus[["Falkestein"]])
-summary(model_gammarus[["Geomar"]])
-summary(model_gammarus[["Trave"]])
+summary(model_gammarus[["Falckenstein beach"]])
+summary(model_gammarus[["Downtown Kiel"]])
+summary(model_gammarus[["Dassower See"]])
 
-p1<- plot_model(model_gammarus[["Falkestein"]], terms = "Date_num",  type = "pred")
+p1<- plot_model(model_gammarus[["Falckenstein beach"]], terms = "Date_num",  type = "pred")
 p1 <- p1 +
   scale_x_continuous(name = "Date", breaks = c(0, 27,58,92,119,155,182,216,239,279,302,329),
-labels = c("April", "May", "June", "July", "August", "September",  "October", "November", "December", "January", "February", "March")) +
+                     labels = c("April", "May", "June", "July", "August", "September",  "October", "November", "December", "January", "February", "March")) +
   theme_bw() + xlab("Sampling months") + ylab("Total species abundance")+ ggtitle("")+ scale_y_continuous(breaks = seq(0, 500 + 50, by = 50))+
   theme( axis.title.x = element_text(size = 12),
-    axis.title.y = element_text(size = 12),
-    axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
-    panel.grid = element_blank(),
-    axis.text = element_text(size = 10, color="black") )
+         axis.title.y = element_text(size = 12),
+         axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
+         panel.grid = element_blank(),
+         axis.text = element_text(size = 10, color="black") )
 layers <- p1$layers
 layers[[1]]$aes_params$colour <- "blue"  
 layers[[2]]$aes_params$fill <- "royalblue1"  
 p1$layers <- layers
+p1 <- p1 + geom_point(data = abundance_data1[abundance_data1$location=="Falckenstein beach",], aes(x = Date_num, y = Abundance), colour = "blue")
 
-p2<- plot_model(model_gammarus[["Geomar"]], terms = "Date_num",  type = "pred", line.color = "blue")
+
+p2<- plot_model(model_gammarus[["Downtown Kiel"]], terms = "Date_num",  type = "pred", line.color = "blue")
 p2 <- p2 +
   scale_x_continuous(name = "Date", breaks = c(0, 27,58,92,119,155,182,216,239,279,302,329),
                      labels = c("April", "May", "June", "July", "August", "September",  "October", "November", "December", "January", "February", "March")) +
@@ -424,9 +449,10 @@ p2 <- p2 +
          panel.grid = element_blank(),
          axis.text = element_text(size = 10, color="black") ) 
 p2$layers <- layers
+p2 <- p2 + geom_point(data = abundance_data1[abundance_data1$location=="Downtown Kiel",], aes(x = Date_num, y = Abundance), colour = "blue")
 
 
-p3<- plot_model(model_gammarus[["Trave"]], terms = "Date_num",  type = "pred")
+p3<- plot_model(model_gammarus[["Dassower See"]], terms = "Date_num",  type = "pred")
 p3 <- p3 +
   scale_x_continuous(name = "Date", breaks = c(0, 27,58,92,119,155,182,216,239,279,302,329),
                      labels = c("April", "May", "June", "July", "August", "September",  "October", "November", "December", "January", "February", "March")) +
@@ -439,6 +465,7 @@ p3 <- p3 +
          panel.grid = element_blank(),
          axis.text = element_text(size = 10, color="black") ) 
 p3$layers <- layers
+p3 <- p3 + geom_point(data = abundance_data1[abundance_data1$location=="Dassower See",], aes(x = Date_num, y = Abundance), colour = "blue")
 
 
 # Model Richness ----
@@ -451,6 +478,7 @@ richness_data$Date_num <- as.numeric(difftime(richness_data$`Sampling Date`,
                                               min(richness_data$`Sampling Date`),
                                               units = "days"))
 
+richness_data$location  <- as.character(richness_data$location)
 model_rich <- list()
 for (loc in unique(richness_data$location)) {
   rich_data <- filter(richness_data, location == loc)
@@ -460,19 +488,19 @@ for (loc in unique(richness_data$location)) {
   colnames(rich_data)[1] <- "Date"
   rich_data$Date <- as.factor(rich_data$Date)
   rich_data$Date <- as.Date(as.character(rich_data$Date))
-
+  
   model_rich[[loc]] <- gam(Richness ~ s(Date_num, bs = "cs") + Year, 
-                               data = rich_data, 
-                               family = nb())
+                           data = rich_data, 
+                           family = nb())
 }
 
-summary(model_rich[["Falkestein"]])
-summary(model_rich[["Geomar"]])
-summary(model_rich[["Trave"]])
+summary(model_rich[["Falckenstein beach"]])
+summary(model_rich[["Downtown Kiel"]])
+summary(model_rich[["Dassower See"]])
 
 
 
-p4<- plot_model(model_rich[["Falkestein"]], terms = "Date_num",  type = "pred")
+p4<- plot_model(model_rich[["Falckenstein beach"]], terms = "Date_num",  type = "pred")
 p4 <- p4 +
   scale_x_continuous(name = "Date", breaks = c(0, 27,58,92,119,155,182,216,239,279,302,329),
                      labels = c("April", "May", "June", "July", "August", "September",  "October", "November", "December", "January", "February", "March")) +
@@ -485,8 +513,9 @@ p4 <- p4 +
          panel.grid = element_blank(),
          axis.text = element_text(size = 10, color="black") )
 p4$layers <- layers
+p4 <- p4 + geom_point(data = richness_data[richness_data$location=="Falckenstein beach",], aes(x = Date_num, y = Richness), colour = "blue")
 
-p5<- plot_model(model_rich[["Geomar"]], terms = "Date_num",  type = "pred", line.color = "blue")
+p5<- plot_model(model_rich[["Downtown Kiel"]], terms = "Date_num",  type = "pred", line.color = "blue")
 p5 <- p5 +
   scale_x_continuous(name = "Date", breaks = c(0, 27,58,92,119,155,182,216,239,279,302,329),
                      labels = c("April", "May", "June", "July", "August", "September",  "October", "November", "December", "January", "February", "March")) +
@@ -499,9 +528,10 @@ p5 <- p5 +
          panel.grid = element_blank(),
          axis.text = element_text(size = 10, color="black") ) 
 p5$layers <- layers
+p5 <- p5 + geom_point(data = richness_data[richness_data$location=="Downtown Kiel",], aes(x = Date_num, y = Richness), colour = "blue")
 
 
-p6<- plot_model(model_rich[["Trave"]], terms = "Date_num",  type = "pred")
+p6<- plot_model(model_rich[["Dassower See"]], terms = "Date_num",  type = "pred")
 p6 <- p6 +
   scale_x_continuous(name = "Date", breaks = c(0, 27,58,92,119,155,182,216,239,279,302,329),
                      labels = c("April", "May", "June", "July", "August", "September",  "October", "November", "December", "January", "February", "March")) +
@@ -514,10 +544,12 @@ p6 <- p6 +
          panel.grid = element_blank(),
          axis.text = element_text(size = 10, color="black") ) 
 p6$layers <- layers
+p6 <- p6 + geom_point(data = richness_data[richness_data$location=="Dassower See",], aes(x = Date_num, y = Richness), colour = "blue")
 
 
-
-p1 +p2+p3+
+remotes::install_github("thomasp85/patchwork")
+remotes::install_github("tidyverse/ggplot2", ref = remotes::github_pull("5592"))
+p1 +p2+p3 +
   p4+p5+p6
 
 
@@ -535,7 +567,7 @@ data12 <- data12 %>% filter(Abundance > 0)
 
 data11$`Sampling Date` <- as.Date(data11$`Sampling Date`, format = "%d/%m/%Y")
 data11$`Sampling month` <- factor(data11$`Sampling month`, levels = c("April", "May", "June", "July", "August", "September",
-                                                                  "October", "November", "December", "January", "February", "March"))
+                                                                      "October", "November", "December", "January", "February", "March"))
 data11$Year <- format(data11$`Sampling Date`, "%Y")
 
 
@@ -564,9 +596,20 @@ local_dat1$Date <- as.factor(local_dat1$Date)
 local_dat1$Date <- as.Date(as.character(local_dat1$Date))
 local_dat1$Date_num <- as.numeric(difftime(local_dat1$Date, min(local_dat1$Date), units = "days"))
 model_abun[["Trave_alien"]] <- gam(Abundance ~ s(Date_num, bs = "cs", k=10) + Year, 
-                         data = local_dat1, 
-                         family = nb())
+                                   data = local_dat1, 
+                                   family = nb())
 # ----
+abun_data$location[abun_data$location=="Falkestein"] <- "Falckenstein beach"
+abun_data$location[abun_data$location=="Geomar"] <- "Downtown Kiel"
+abun_data$location[abun_data$location=="Trave"] <- "Dassower See"
+
+abun_data <- abun_data %>% filter(Abundance > 0) %>%
+  group_by(`Sampling Date`, location, Year) %>%
+  summarize(Abundance = sum(Abundance), .groups = 'drop')
+abun_data$Year <- as.factor(abun_data$Year)
+abun_data$Date_num <- as.numeric(difftime(abun_data$`Sampling Date`,
+                                              min(abun_data$`Sampling Date`),
+                                              units = "days"))
 
 model_abun <- list()
 for (loc in unique(abundance_data$location)) {
@@ -580,17 +623,17 @@ for (loc in unique(abundance_data$location)) {
   local_data1$Date <- as.Date(as.character(local_data1$Date))
   local_data1$Date_num <- as.numeric(difftime(local_data1$Date, min(local_data1$Date), units = "days"))
   
-  model_abun[[loc]] <- gam(Abundance ~ s(Date_num, bs = "cs", k=10) + Year, 
-                               data = local_data1, 
-                               family = nb())
+  model_abun[[loc]] <- gam(Abundance ~ s(Date_num, bs = "cs", k=5) + Year, 
+                           data = local_data1, 
+                           family = nb())
 }
-summary(model_abun[["Falkestein"]])
-summary(model_abun[["Geomar"]])
-summary(model_abun[["Trave"]])
+summary(model_abun[["Falckenstein beach"]])
+summary(model_abun[["Downtown Kiel"]])
+summary(model_abun[["Dassower See"]])
 summary(model_abun[["Trave_alien"]])
 
 
-p7<- plot_model(model_abun[["Falkestein"]], terms = "Date_num",  type = "pred")
+p7<- plot_model(model_abun[["Falckenstein beach"]], terms = "Date_num",  type = "pred")
 p7 <- p7 +
   scale_x_continuous(name = "Date", breaks = c(0, 27,58,92,119,155,182,216,239,279,302,329),
                      labels = c("April", "May", "June", "July", "August", "September",  "October", "November", "December", "January", "February", "March")) +
@@ -607,9 +650,10 @@ layers <- p7$layers
 layers[[1]]$aes_params$colour <- "blue"  
 layers[[2]]$aes_params$fill <- "royalblue1"  
 p7$layers <- layers
+p7 <- p7 + geom_point(data = abun_data[abun_data$location=="Falckenstein beach",], aes(x = Date_num, y = Abundance), colour = "blue")
 
 
-p8<- plot_model(model_abun[["Geomar"]], terms = "Date_num",  type = "pred", line.color = "blue")
+p8<- plot_model(model_abun[["Downtown Kiel"]], terms = "Date_num",  type = "pred", line.color = "blue")
 p8 <- p8 +
   scale_x_continuous(name = "Date", breaks = c(0, 27,58,92,119,155,182,216,239,279,302,329),
                      labels = c("April", "May", "June", "July", "August", "September",  "October", "November", "December", "January", "February", "March")) +
@@ -626,8 +670,9 @@ layers <- p8$layers
 layers[[1]]$aes_params$colour <- "blue"  
 layers[[2]]$aes_params$fill <- "royalblue1"  
 p8$layers <- layers
+p8 <- p8 + geom_point(data = abun_data[abun_data$location=="Downtown Kiel",], aes(x = Date_num, y = Abundance), colour = "blue")
 
-p9<- plot_model(model_abun[["Trave"]], terms = "Date_num",  type = "pred")
+p9<- plot_model(model_abun[["Dassower See"]], terms = "Date_num",  type = "pred")
 p9 <- p9 +
   scale_x_continuous(name = "Date", breaks = c(0, 27,58,92,119,155,182,216,239,279,302,329),
                      labels = c("April", "May", "June", "July", "August", "September",  "October", "November", "December", "January", "February", "March")) +
@@ -644,6 +689,7 @@ layers <- p9$layers
 layers[[1]]$aes_params$colour <- "blue"  
 layers[[2]]$aes_params$fill <- "royalblue1"  
 p9$layers <- layers
+p9 <- p9 + geom_point(data = abun_data[abun_data$location=="Dassower See",], aes(x = Date_num, y = Abundance), colour = "blue")
 
 p10<- plot_model(model_abun[["Trave_alien"]], terms = "Date_num",  type = "pred")
 p10 <- p10 +
@@ -661,6 +707,7 @@ lay <- p10$layers
 lay[[1]]$aes_params$colour <- "red"  
 lay[[2]]$aes_params$fill <- "indianred2"  
 p10$lay <- lay
+p10 <- p10 + geom_point(data = local_dat1, aes(x = Date_num, y = Abundance), colour = "red")
 
 
 p7 +p8+p9+ p10
@@ -749,6 +796,16 @@ for (loc in unique(extra2$location)) {
     layers[[2]]$aes_params$fill <- "royalblue1"  
     p$layers <- layers
     
+    if(pred =="Salinity"){
+  p <- p + geom_point(data = extra3, aes(x = Salinity, y = Total_abundance), colour = "blue")
+    } else if(pred =="Oxygen"){
+  p <- p + geom_point(data = extra3, aes(x = Oxygen, y = Total_abundance), colour = "blue")
+    }else if(pred =="Temperature"){
+  p <- p + geom_point(data = extra3, aes(x = Temperature, y = Total_abundance), colour = "blue")
+    }else if(pred =="pH"){
+  p <- p + geom_point(data = extra3, aes(x = pH, y = Total_abundance), colour = "blue")
+    }
+    
     plot_name <- paste(loc, pred, sep = "_")
     plots[[plot_name]] <- p
   }
@@ -762,12 +819,12 @@ plots[[8]]+ plots[[12]]+ plots[[4]] # pH
 
 
 plot_distribution <-  (plots[[6]] + plots[[10]] + plots[[2]])/ 
-(plots[[8]] + plots[[12]] + plots[[4]])/ 
+  (plots[[8]] + plots[[12]] + plots[[4]])/ 
   (plots[[5]] + plots[[9]] + plots[[1]]) / 
   (plots[[7]] + plots[[11]] + plots[[3]]) 
-  
+
 plot_distribution
-ggsave("combined_plot_e.svg", 
+ggsave("Figure7.svg", 
        plot = plot_distribution, 
        width = 12, height = 12, device = "svg")
 
@@ -779,4 +836,40 @@ smooth_p_values <- model_summary$s.pv
 corrected_p_values <- p.adjust(smooth_p_values, method = "holm")
 
 
+###### Ordination  ############
 
+extra2
+extra3 <- na.omit(extra2)
+
+rda <- vegan::rda(extra3$Total_abundance ~ Salinity + Oxygen + Temperature + pH, data = extra3)
+summary(rda)
+plot(rda)
+permutation_test <- anova.cca(rda, permutations = 999)
+print(permutation_test
+marginal_permutation_test <- anova.cca(rda, by = "margin", permutations = 999)
+marginal_permutation_test
+
+rda_scores <- scores(rda, display = "sites")
+env_scores <- scores(rda, display = "bp")
+
+rda_scores_df <- as.data.frame(rda_scores)
+env_scores_df <- as.data.frame(env_scores)
+
+rda_scores_df$location <- extra3$location
+
+rda_plot <- ggplot(rda_scores_df, aes(x = RDA1, y = PC1, color = location)) +
+  geom_point(size = 3) +
+  geom_segment(data = env_scores_df, aes(x = 0, y = 0, xend = RDA1, yend = PC1),
+               arrow = arrow(length = unit(0.25, "cm")), color = "black") +
+  geom_text(data = env_scores_df, aes(x = RDA1, y = PC1, label = rownames(env_scores_df)),
+            vjust = 1.5, hjust = 1.5, color = "black") +
+  theme_bw() + geom_hline(yintercept=0, linetype="dashed", size=0.5)+ 
+  geom_vline(xintercept=0, linetype="dashed", size=0.5)+ 
+  labs(  x = "RDA1",  y = "PC1",
+    color = "Location" ) +
+  theme(
+    axis.title = element_text(size = 12),
+    axis.text = element_text(size = 10, color = "black"),
+    plot.title = element_text(size = 14, hjust = 0.5)
+  )
+combined_plot <- ggMarginal(rda_plot, type = "histogram", bins = 30, groupColour = TRUE, groupFill = TRUE)
